c88c5f28ca21701f1c727562b66fbd0c107c150a
SgLy <775150558@qq.com>
2018-09-13 02:40:54 +0800

    M: change parse api name

--- utils/api.js
+++ utils/api.js
@@ -4,7 +4,7 @@ const { PROTO, SERVER, PORT } = require('./server_config');
 module.exports = {
   parse: text => {
     return wx.request({
-      url: `${PROTO}://${SERVER}:${PORT}/api/translate`,
+      url: `${PROTO}://${SERVER}:${PORT}/api/parse`,
       method: 'POST',
       data: { text }
     });
