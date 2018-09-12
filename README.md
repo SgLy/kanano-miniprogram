3a2bfaf6f5a7980facea4859d8bf5507576bc0a0
SgLy <775150558@qq.com>
2018-09-12 13:21:00 +0800

    A: init env

---
+++ README.md
@@ -0,0 +1,7 @@
+### kanano-miniprogram
+
+Wechat miniprogram as a kana annotator and memo
+
+#### Run guide
+
+Open with miniprogram devtool, need no compilation.
---
+++ wx_promisify.js
@@ -0,0 +1,18 @@
+const wx_p = (() => {
+  const _wx = {};
+  const ret = {};
+  Object.keys(wx).forEach(n => {
+    _wx[n] = wx[n];
+    ret[n] = (args = {}) => new Promise((resolve, reject) => {
+      args.success = (res) => { resolve(res); };
+      args.fail = (res) => { reject(res); };
+      _wx[n](args);
+    });
+  });
+  return ret;
+})();
+
+export default {
+  wxSync: wx,
+  wx: wx_p
+};
