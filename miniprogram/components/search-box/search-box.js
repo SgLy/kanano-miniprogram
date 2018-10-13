5f98c9b119591c19768d3f7ca8f73e757a85a850
SgLy <775150558@qq.com>
2018-10-13 23:04:44 +0800

    search box handle height

--- miniprogram/components/search-box/search-box.js
+++ miniprogram/components/search-box/search-box.js
@@ -1,18 +1,18 @@
+const app = getApp();
+
 Component({
-  properties: {
-    height: {
-      type: Number,
-      value: 64
-    }
-  },
   data: {
     atTop: true
   },
   ready() {
+    // observer to switch box-shadow
     let observer = this.createIntersectionObserver();
     observer.relativeToViewport({ top: 0 });
     observer.observe('.this-will-be-on-top-of-page', res => {
       this.setData({ atTop: res.intersectionRatio < 1 });
     });
+
+    // height
+    this.setData({ height: app.navigationBarHeight });
   }
 });
