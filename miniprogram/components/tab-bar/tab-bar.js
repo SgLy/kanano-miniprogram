1069bf1647cd1b6c13f26ed1c8cdd9c973d1a652
SgLy <775150558@qq.com>
2018-10-15 00:39:20 +0800

    add tab bar icon

--- miniprogram/components/tab-bar/tab-bar.js
+++ miniprogram/components/tab-bar/tab-bar.js
@@ -1,18 +1,25 @@
+const PREFIX = '../../assets/images/tab-icons';
 const TABS = [
   {
     name: '列表',
     url: '/pages/index/index',
-    showOnlyMatched: false
+    showOnlyMatched: false,
+    icon: `${PREFIX}/list.svg`,
+    matchedIcon: `${PREFIX}/list-matched.svg`
   },
   {
     name: '标注',
     url: '/pages/text/text',
-    showOnlyMatched: true
+    showOnlyMatched: true,
+    icon: `${PREFIX}/text.svg`,
+    matchedIcon: `${PREFIX}/text-matched.svg`
   },
   {
     name: '设置',
     url: '/pages/setting/setting',
-    showOnlyMatched: false
+    showOnlyMatched: false,
+    icon: `${PREFIX}/setting.svg`,
+    matchedIcon: `${PREFIX}/setting-matched.svg`
   }
 ];
 
@@ -28,6 +35,8 @@ Component({
       route = '/' + route;
     }
     const matched = TABS.findIndex(tab => route.search(tab.url) !== -1);
+    TABS.forEach(t => t.matched = false);
+    TABS[matched].matched = true;
     this.setData({
       tabs: TABS,
       currentTab: matched
@@ -35,7 +44,9 @@ Component({
   },
   ready() {
     // observer to switch box-shadow
-    let observer = this.createIntersectionObserver();
+    let observer = this.createIntersectionObserver({
+      initialRatio: 1
+    });
     observer.relativeToViewport({ bottom: 0 });
     observer.observe('.this-will-be-on-bottom-of-page', res => {
       this.setData({ atBottom: res.intersectionRatio > 0 });
